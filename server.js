b4966b5b5cd6ba96fa9cbcf1f4e892444bbd1d98
MEwing <masandraewing@gmail.com>
2023-01-30 18:44:49 -0500

    Saving point for end of January 30th

--- server.js
+++ server.js
@@ -30,9 +30,6 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// app.get('/api/notes', (req, res) => {
-//   res.json()
-// })
 
 // TO DO - Change to Heroku url when ready
 app.listen(PORT, () => 
