7454151e964b85aa1290eaba345e5113d516f8dc
MEwing <masandraewing@gmail.com>
2023-01-31 14:02:15 -0500

    Testing out Heroku deployment. Saving point in case I break the code

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('*', (req, res) =>
 
 // TO DO - Change to Heroku url when ready
 app.listen(PORT, () => 
-    console.log(`App is listening at http://localhost:${PORT} 🚀`)
+    console.log(`App is listening at http://localhost:${PORT}`)
 );
 
 /* 
@@ -63,18 +63,22 @@ WHEN I open the Note Taker
 THEN I am presented with a landing page with a link to a notes page
 
 WHEN I click on the link to the notes page
-THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+THEN I am presented with a page with existing notes listed in the left-hand column, 
+plus empty fields to enter a new note title and the note’s text in the right-hand 
+column
 
 WHEN I enter a new note title and the note’s text
 THEN a Save icon appears in the navigation at the top of the page
 
 WHEN I click on the Save icon
-THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+THEN the new note I have entered is saved and appears in the left-hand column 
+with the other existing notes
 
 WHEN I click on an existing note in the list in the left-hand column
 THEN that note appears in the right-hand column
 
 WHEN I click on the Write icon in the navigation at the top of the page
-THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+THEN I am presented with empty fields to enter a new note title and the note’s 
+text in the right-hand column
 
 */
